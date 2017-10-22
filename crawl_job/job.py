7540a0cb576b85e1cdb9f43d8f8568b3155df994
zhouhaocheng <1711541298@qq.com>
2017-10-22 18:53:41 +0800

    爬取51job招聘信息

---
+++ crawl_job/job.py
@@ -0,0 +1,108 @@
+# -*- coding:utf-8 -*-
+
+#导入模块
+import MySQLdb
+import re
+import urllib2
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+class Item(object):
+    JobName = None
+    CompanyName = None
+    WorkPlace = None
+    Salary = None
+    Time1 =None
+
+class Job(object):
+    def __init__(self,job):
+        self.job = job
+        self.baseurl = 'http://search.51job.com/list/030200,000000,0000,00,9,99,'
+        self.url = self.baseurl + self.job + ',2,1.html'
+        self.firstPage = self.Getpage(self.url)
+
+
+
+
+    def Getpage(self,url):
+        response = urllib2.urlopen(url)
+        html = response.read()
+        return html
+
+
+    def getUrls(self, firstPage):
+        ''' 获取urls列表 '''
+        s = '共(.*?)页'.decode('utf-8')
+        defPage = re.compile(s)
+        # print self.firstPage.decode('gbk')
+        fullPage = re.search(defPage, self.firstPage.decode('gbk')).group(1)
+        # print fullPage
+        # fullPage = 65
+        urls = []
+        ul = self.url.split(',')
+        for page in range(1, int(fullPage) + 1):
+            ul[-1] = str(page) + '.html'
+            url = ','.join(ul)
+            urls.append(url)
+        return urls
+
+
+
+
+
+
+#创建数据库连接
+conn = MySQLdb.connect(
+    host='192.168.100.10',
+    port=3306,
+    user='craw123',
+    passwd='craw123',
+    db='db',
+    charset='utf8')
+cur = conn.cursor()
+
+
+#获取源码
+def get_info(page):
+    url ='http://search.51job.com/list/000000,000000,0000,00,9,99,python,2,'+ str(page)+'.html'
+    a = urllib2.urlopen(url)
+    html = a.read().decode('gbk')       # 读取源代码并转为unicode
+    return html
+
+#获取爬取的内容
+def get(html):
+    #利用正则提取出需要的信息
+    reg = re.compile(r'class="t1 ">.*? <a target="_blank" title="(.*?)".*? <span class="t2"><a target="_blank" title="(.*?)".*?<span class="t3">(.*?)</span>.*?<span class="t4">(.*?)</span>.*? <span class="t5">(.*?)</span>',re.S) #匹配换行符
+    items = re.findall(reg,html)
+    return items
+
+#将爬取的内容写进文件
+for b in range(1,20):  #设置爬取的页数
+    html = get_info(b)
+    print ("正在爬取第" + str(b) + "页的内容......")
+    for num in get(html):
+        filename = u'51job招聘信息.txt'
+        with open(filename,'a') as f:
+            f.write(num[0] + '\t' + num[1] + '\t' + num[2] + '\t' + num[3] + '\t' + num[4] + '\n' )
+            f.close
+
+#将爬取的内容写进数据库
+for b in range(1,20):  #设置爬取的页数
+    html = get_info(b)
+    for num in get(html):
+        JobName = num[0].encode('utf-8')
+        CompanyName = num[1].encode('utf-8')
+        WorkPlace = num[2].encode('utf-8')
+        Salary = num[3].encode('utf-8')
+        Time1 = num[4].encode('utf-8')
+        cur.execute("INSERT INTO pythonjobs(Jobname, CompanyName, WorkPlace, Salary, Time1) values(%s,%s,%s,%s,%s)",
+        (JobName, CompanyName, WorkPlace, Salary, Time1))
+
+#关闭连接
+cur.close()
+conn.commit()
+conn.close()
+
+
---
+++ crawl_job/job_new.py
@@ -0,0 +1,117 @@
+# -*- coding:utf-8 -*-
+import urllib2
+from bs4 import BeautifulSoup
+import re
+import MySQLdb
+
+
+class Item(object):
+    '''定义Item类'''
+    JobName = None
+    CompanyName = None
+    WorkPlace = None
+    Salary = None
+    Time = None
+
+
+class getJobInfo(object):
+    """get www.51job.com Info"""
+
+    def __init__(self, job):
+        self.job = job
+        self.baseurl = 'http://search.51job.com/list/030200,000000,0000,00,9,99,'
+        self.url = self.baseurl + self.job + ',2,1.html'
+        self.firstPage = self.getPage(self.url)
+        self.urls = self.getUrls(self.firstPage)
+        self.items = self.spider(self.urls)
+        # self.pipelines2file(self.items)
+        self.pipelines2mysql(self.items)
+
+    def getPage(self, url):
+        ''' 获取网页源代码 '''
+        response = urllib2.urlopen(url)
+        html = response.read()
+        return html
+
+    def getUrls(self, firstPage):
+        ''' 获取urls列表 '''
+        s = '共(.*?)页'.decode('utf-8')
+        defPage = re.compile(s)
+        # print self.firstPage.decode('gbk')
+        fullPage = re.search(defPage, self.firstPage.decode('gbk')).group(1)
+        # print fullPage
+        # fullPage = 65
+        urls = []
+        ul = self.url.split(',')
+        for page in range(1, int(fullPage) + 1):
+            ul[-1] = str(page) + '.html'
+            url = ','.join(ul)
+            urls.append(url)
+        return urls
+
+    def spider(self, urls):
+        ''' 爬取item类定义的信息'''
+        items = []
+        for url in urls:
+            html = self.getPage(url)
+            soup = BeautifulSoup(html, 'lxml')
+            divs1 = soup.find('div', attrs={'id': 'resultList'})
+            divlists = divs1.find_all('div', attrs={'class': 'el'})
+            for div in divlists:
+                try:
+                    item = Item()
+                    print unicode(div.find('p', attrs={'class': 't1'}).get_text().strip())
+                    item.JobName = unicode(div.find('p', attrs={'class': 't1'}).get_text().strip())
+                    print unicode(div.find('span', attrs={'class': 't2'}).a.get_text())
+                    item.CompanyName = unicode(div.find('span', attrs={'class': 't2'}).a.get_text())
+                    print unicode(div.find('span', attrs={'class': 't3'}).get_text())
+                    item.WorkPlace = unicode(div.find('span', attrs={'class': 't3'}).get_text())
+                    print unicode(div.find('span', attrs={'class': 't4'}).get_text())
+                    item.Salary = unicode(div.find('span', attrs={'class': 't4'}).get_text())
+                    print unicode(div.find('span', attrs={'class': 't5'}).get_text())
+                    item.Time = unicode(div.find('span', attrs={'class': 't5'}).get_text())
+                except:
+                    pass
+                    continue
+                else:
+                    items.append(item)
+        return items
+
+
+
+    def pipelines2file(self, items):
+        ''' 把爬取到的数据存储到文件'''
+        # Job = self.url.split(',')[-3]
+        fileName = self.job + u'的招聘信息.txt'
+        # fileName = u'51Job招聘信息.txt'
+        with open(fileName, 'w') as fp:
+            fp.write('%-35s \t| %-30s \t| %-20s \t| %20s \t| %20s \n' % ('职位名', '公司名', '工作地点', '薪资', '发布时间'))
+            for item in items:
+                fp.write('%-40s \t| %-40s \t| %-20s \t| %20s \t| %20s \n' % (
+                item.JobName.encode('utf-8'), item.CompanyName.encode('utf-8'), item.WorkPlace.encode('utf-8'),
+                item.Salary.encode('utf-8'), item.Time.encode('utf-8')))
+
+    def pipelines2mysql(self, items):
+        ''' 把爬取到的数据存储到数据库'''
+        conn = MySQLdb.connect(
+            host='192.168.10.10',
+            port=3306,
+            user='crawl123',
+            passwd='crawl123',
+            db='scrapyDB',
+            charset='utf8')
+        cur = conn.cursor()
+        for item in items:
+            JobName = item.JobName.encode('utf-8')
+            CompanyName = item.CompanyName.encode('utf-8')
+            WorkPlace = item.WorkPlace.encode('utf-8')
+            Salary = item.Salary.encode('utf-8')
+            Time1 = item.Time.encode('utf-8')
+            cur.execute("INSERT INTO pythonjobs(JobName, CompanyName, WorkPlace, Salary, Time1) values(%s,%s,%s,%s,%s)",
+                        (JobName, CompanyName, WorkPlace, Salary, Time1))
+        cur.close()
+        conn.commit()
+        conn.close()
+
+
+JI = getJobInfo('python')
